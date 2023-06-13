4027507dad125cc840d1bab45983c2dde3016b09
FREEGREAT <vladdmitrenko3009@gmail.com>
2023-06-14 01:38:37 +0300

    output data to console

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,63 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import Navbar from './components/Navbar'
 import db from './config/firebase'
 import './App.css'
-import { collection, onSnapshot } from '@firebase/firestore';
+import { 
+  collection, 
+  onSnapshot,  
+  getDocs, 
+  doc,
+  query  } from '@firebase/firestore';
+
+
+async function queryDoc(){
+  const querr = query(
+    collection(db, '/groups')
+  );
+
+  const querySnapshot = await getDocs(querr);
+  const allDocs = querySnapshot.forEach((snap) => {
+    console.log(`Document ${snap.id} contains ${JSON.stringify(snap.data())}`);
+  });
+
+}
+
+/*async function queryDoc() {
+  const collectionPath = 'groups';
+  const segments = collectionPath.split('/');
+  const collectionRef = collection(db, ...segments);
+  const querySnapshot = await getDocs(collectionRef);
+  var aa
+  querySnapshot.forEach((doc) => {
+    (`Document ${doc.id} contains ${JSON.stringify(doc.data())}`);
+  });
+
+}*/
+
+
 
 function App() {
-  useEffect(() =>{
-    onSnapshot(collection(db,'groups'),(onSnapshot) =>{
-        console.log(onSnapshot.docs.map(doc => doc.data))
-    });
-  })
+  const group = collection(db, `Groups`)
+  const zz =  queryDoc();
+  /*onSnapshot(group, onSnapshot => {
+    if(onSnapshot.exists()){
+      const docData = onSnapshot.data();
+      console.log(`Zalupa  is ${JSON.stringify(docData)}`);
+    }
+  });*/
+
+ 
+
   const [showSidebar, setShowBar] = useState(false)
   return (
       <div className='App'>
         <header>
           <GiHamburgerMenu onClick={() => setShowBar(!showSidebar)} />
         </header>
-
          < Navbar show={showSidebar} />
       </div>
+      
   )
 }
 
